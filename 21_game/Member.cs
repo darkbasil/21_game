c08f74cbebba33454b35b03d5a7605ac70a523f5
darkbasil <basil_triste@mail.ru>
2013-10-04 18:38:01 +0800

    Create class Player

--- 21_game/Member.cs
+++ 21_game/Member.cs
@@ -4,6 +4,6 @@ namespace _21_game
 {
     public abstract class Member
     {
-        private IList<Card> Hand { get; set; } 
+        public IList<Card> Hand { get; set; } 
     }
 }
---
+++ 21_game/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _21_game
+{
+	public class Player : Member
+	{
+		const double StartCash = 500;
+
+		private List<Card>  _playersHand = new List<Card>();
+		private Double _cash = StartCash;
+		
+		public List<Card> Hand
+		{
+			get
+			{
+				return _playersHand;
+			}
+		}
+
+		public Double Cash
+		{
+			get
+			{
+				return _cash;
+			}
+		}
+	}
+}
