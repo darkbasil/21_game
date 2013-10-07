07a1b79a69cd5b45cc3bb903c2949873501deb66
darkbasil <basil_triste@mail.ru>
2013-10-07 11:42:41 +0800

    create class Points; create class Card
    Class Points calculate sum points for Player and Dealer.
    
    Class Card keeps Color and Rank (1 .. 13) for each card.

---
+++ 21_game/Card.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace _21_game
+{
+	public class Card
+	{
+		private string _color { get; set; }
+		private int _rank { get; set; }
+
+		public string Color
+		{
+			get
+			{
+				return _color;
+			}
+		}
+
+		public int Rank
+		{
+			get
+			{
+				return _rank;
+			}
+		}
+	}
+}
---
+++ 21_game/Points.cs
@@ -0,0 +1,34 @@
+﻿namespace _21_game
+{
+	public class Points
+	{
+		public int Calculate(Member member)
+		{
+			int _sumPoints = 0;
+			foreach (var card in member.Hand)
+			{
+				if (card.Rank != 1)
+				{
+					if (card.Rank < 10)
+					{
+						_sumPoints += card.Rank;
+					}
+					else
+					{
+						_sumPoints += 10;
+					}
+				}
+				else if (_sumPoints + 11 <= 21)
+				{
+					_sumPoints += 11;
+				}
+				else
+				{
+					_sumPoints += 1;
+				}
+				
+			}
+			return _sumPoints;
+		}
+	}
+}
